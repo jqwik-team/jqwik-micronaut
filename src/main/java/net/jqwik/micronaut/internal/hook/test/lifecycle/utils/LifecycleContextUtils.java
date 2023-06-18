9573543914d0be4d318b3834b2aacb50c39e77a7
Nahuel <12597182+Nahuel92@users.noreply.github.com>
2023-06-18 11:27:37 -0300

    refactor(jqwik-micronaut): Cleaned up logic in `LifecycleContextUtils`. .
    Signed-off-by: Nahuel <12597182+Nahuel92@users.noreply.github.com>

--- src/main/java/net/jqwik/micronaut/internal/hook/test/lifecycle/utils/LifecycleContextUtils.java
+++ src/main/java/net/jqwik/micronaut/internal/hook/test/lifecycle/utils/LifecycleContextUtils.java
@@ -1,20 +1,21 @@
 package net.jqwik.micronaut.internal.hook.test.lifecycle.utils;
 
 import net.jqwik.api.lifecycle.LifecycleContext;
-import net.jqwik.api.lifecycle.TryLifecycleContext;
 import net.jqwik.micronaut.JqwikMicronautTest;
 
 public class LifecycleContextUtils {
+    private static final String ANNOTATION_NOT_FOUND =
+            "Expected '@JqwikMicronautTest' to be present in the test class!";
+
     public static boolean isPerProperty(final LifecycleContext context) {
+        return !isPerTry(context);
+    }
+
+    public static boolean isPerTry(final LifecycleContext context) {
         return context.findAnnotationsInContainer(JqwikMicronautTest.class)
                       .stream()
                       .map(JqwikMicronautTest::perTry)
-                      .map(e -> !e)
                       .findFirst()
-                      .orElseThrow(() -> new IllegalArgumentException("Expected 'perTry' to contain a value!"));
-    }
-
-    public static boolean isPerTry(final TryLifecycleContext context) {
-        return !isPerProperty(context);
+                      .orElseThrow(() -> new IllegalArgumentException(ANNOTATION_NOT_FOUND));
     }
 }
